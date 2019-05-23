a120222175dc55cfde38568d52de4bb4162d1998
Shiraj Sayed <shirajsayed13@gmail.com>
2019-05-23 22:08:00 +0530

    Add all required dependencies and layout files

--- app/src/main/java/com/example/jsonplaceholdertypicode/ActivityPosts.kt
+++ app/src/main/java/com/example/jsonplaceholdertypicode/ActivityPosts.kt
@@ -3,10 +3,10 @@ package com.example.jsonplaceholdertypicode
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ActivityPosts : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_posts)
     }
 }
